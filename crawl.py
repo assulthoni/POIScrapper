053dfcb04cf2bbb488e9981c3d1a00a45818fd29
Ahmad Shohibus Sulthoni <thoni.shohibus@gmail.com>
2022-04-26 01:12:59 +0700

    all in one

---
+++ crawl.py
@@ -0,0 +1,134 @@
+import os
+import ast
+import time
+import psutil
+from selenium import webdriver
+from itertools import zip_longest
+import pandas as pd
+import random
+from dotenv import load_dotenv
+
+random.seed(47)
+load_dotenv()
+
+POIs = ["school", "restaurant", "mall", "office", "mart", "halte", "mosque", "gas station"]
+
+def parse_url(latitude, longitude, poi):
+    zoom = "16z" # this is zoom level set to 200 m radius
+    base_url = "https://maps.google.com/maps/search/"
+    url = base_url + poi.replace(' ', '+') + "/"
+    latlong = f"@{latitude},{longitude},{zoom}/"
+    return url + latlong + '?hl=en'
+
+def create_browser(headless=True, sandbox=False, disable_dev_shm=False):
+    """
+    return : chromedriver browser selenium
+    """
+    try:
+        CHROME_PATH = os.getenv('CHROME_PATH')
+        prefs = {
+            "profile.managed_default_content_settings.images" : 2,
+            # "profile.default_content_setting_values.geolocation" : 2
+        }
+        options = webdriver.ChromeOptions()
+        
+        options.add_experimental_option("prefs", prefs)
+        if headless:
+            options.add_argument('--headless')
+        if sandbox:
+            options.add_argument('--no-sandbox')
+        if disable_dev_shm:
+            options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--ignore-certificate-errors')
+        browser = webdriver.Chrome(CHROME_PATH, options=options)
+        return browser
+    except Exception as e:
+        print(e)
+        return None
+
+def kill_chrome():
+    PROCNAME = "chromedriver" # or chromedriver or iedriverserver
+    BROWSERNAME = "chrome"
+    for proc in psutil.process_iter():
+        # check whether the process name matches
+        if proc.name() == PROCNAME or proc.name() == BROWSERNAME:
+            print(f"kill the {PROCNAME} | {BROWSERNAME} process")
+            proc.kill()
+
+def zip_name_review(names, reviews, poi):
+    list_data = []
+    name_review = list(zip_longest(names, reviews))
+    # print(name_review)
+    for name, review in name_review:
+        data = {}
+        data['poi'] = poi
+        data['name'] = name
+        if not review:
+            data['number_review'] = 0
+            data['number_rating'] = 0
+        else:
+            review = review.strip()
+            num_review = review[review.find("(")+1:review.find(")")]
+            num_rating = review.replace(f'({num_review})', '')
+            num_review = change_to_number(num_review)
+            num_rating = change_to_number(num_rating)
+            data['number_review'] = num_review if num_review else 0
+            data['number_rating'] = num_rating if num_rating else 0
+        # print(data)
+        list_data.append(data)
+    return list_data
+
+def change_to_number(str_num):
+    try:
+        str_num = str_num.replace(',', '')
+        return ast.literal_eval(str_num)
+    except Exception as e:
+        return None
+
+def change_zoom(browser):
+    change_js = """
+    var selectBox = document.querySelector("settings-ui").shadowRoot.querySelector("#main").shadowRoot.querySelector("settings-basic-page").shadowRoot.querySelector("settings-appearance-page").shadowRoot.querySelector("#zoomLevel");
+    var changeEvent = new Event("change");
+    selectBox.value = arguments[0];
+    selectBox.dispatchEvent(changeEvent);
+    """
+    browser.get("chrome://settings/")
+    new_zoom = round(25 / 100, 2)
+    browser.execute_script(change_js, new_zoom)
+    return browser
+
+def calculate_score(num_rating, num_review):
+    if num_rating == 0:
+        num_rating = 1
+    if num_review == 0:
+        num_review = 1
+    avg_total = (num_review / 5 ) / (num_review / num_rating) * (num_review)
+    return avg_total
+
+def process(lat, long):
+    headless = True
+    browser = create_browser(headless)
+    if not headless:
+        browser = change_zoom(browser)
+    all_data = []
+    for poi in POIs:
+        url = parse_url(lat, long, poi)
+        browser.get(url)
+        time.sleep(2)   
+        names = browser.find_elements_by_class_name(os.getenv('CLASS_NAMES','qBF1Pd-haAclf'))
+        reviews = browser.find_elements_by_class_name(os.getenv('CLASS_REVIEWS','OEvfgc-wcwwM-haAclf'))
+        names = [name.text for name in names]
+        reviews = [review.text for review in reviews]
+        list_data = zip_name_review(names, reviews, poi)
+        print(list_data)
+        all_data.extend(list_data)
+    browser.close()
+    print(all_data)
+    return all_data
+
+if __name__ == '__main__':
+    lat, long = -6.191022762147621, 106.91646649497352
+    data = process(lat, long)
+    df = pd.DataFrame(data)
+    df.to_csv("all POIs data no zoom.csv", index=False)
+    kill_chrome()
---
+++ test.py
@@ -0,0 +1,9 @@
+from crawl import calculate_score
+
+if __name__ == '__main__':
+    print(calculate_score(0,0))
+    print(calculate_score(0,1))
+    print(calculate_score(1,0))
+    print(calculate_score(5,5))
+    print(calculate_score(5,100))
+    print(calculate_score(3.5,12828))
